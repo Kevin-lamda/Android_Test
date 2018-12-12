fccc87253439060a2b413bb5edded5e28ea0edc1
小鱼艾菲 <alisin5@live.com>
2018-12-13 01:48:23 +0800

    12

--- app/src/main/java/ssw/com/myapplication/BaiduActivity.java
+++ app/src/main/java/ssw/com/myapplication/BaiduActivity.java
@@ -1,49 +1,106 @@
 package ssw.com.myapplication;
 
-import android.app.Activity;
 import android.content.Context;
-import android.os.Build;
+import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.net.http.SslError;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.webkit.CookieManager;
+import android.webkit.SslErrorHandler;
+import android.webkit.WebResourceResponse;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
-import android.widget.TextView;
 import android.widget.Toast;
 
 public class BaiduActivity extends AppCompatActivity {
-    private WebView web;
+    private WebView webView;
+    private String TAG = "BaiduActivity";
+    //private String url = "https://www.baidu.com";
     private String url = "https://wappass.baidu.com/passport/login?sms=1&adapter=3&u=%2F%2Fwappass.baidu.com%2Fv3%2Flogin%2Fapi%2Fauth%2F%3Freturn_type%3D5%26tpl%3Dblockchain%26u%3Dhttps%253A%252F%252Fpet-chain.baidu.com%252Fdata%252Fuser%252Fsign%253Fu%253Dhttps%25253A%25252F%25252Fpet-chain.baidu.com%25252Fchain%25252Fpersonal";
     @Override
-    protected void onCreate(Bundle savedInstanceState){
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_baidu);
-        web = findViewById(R.id.webView);
-        web.loadUrl(url);
-    }
-    public class MyWebViewClient extends WebViewClient {
-        String url;
-        WebView view;
-        Activity activity;
-        TextView textView;
-        public MyWebViewClient(Activity activity)
-        {
-            //  this.textView =textView;
-            this.activity =activity;
-        }
 
-        public boolean shouldOverrideUrlLoading(WebView view, String url) {
-            view.loadUrl(url);
-            return true;
-        }
+        webView = findViewById(R.id.webView);
+        webView.requestFocus();
+        webView.setHorizontalScrollBarEnabled(false);
+        webView.setVerticalScrollBarEnabled(false);
+        WebSettings web = webView.getSettings();
+        web.setJavaScriptEnabled(true);
+        web.setBuiltInZoomControls(true);
+        web.setSupportZoom(true);
+        web.setLayoutAlgorithm(WebSettings.LayoutAlgorithm.NARROW_COLUMNS);
+        web.setUseWideViewPort(true);
+        web.setLoadWithOverviewMode(true);
+        web.setSavePassword(true);
+        web.setSaveFormData(true);
+        webView.loadUrl(url);
+        webView.setWebViewClient(new MyWebViewClient());
+    }
+
+    private class MyWebViewClient extends WebViewClient {
+
+        /**
+         * 在开始加载网页时会回调
+         */
         @Override
-        protected void onPageFinished(WebView view, String url) {
+        public void onPageStarted(WebView view, String url, Bitmap favicon) {
+            super.onPageStarted(view, url, favicon);
+            Log.d(TAG,"onPageStarted");
+        }
+        /**
+         * 在结束加载网页时会回调
+         */
+        public void onPageFinished(WebView view, String url) {
             CookieManager cookieManager = CookieManager.getInstance();
             String CookieStr = cookieManager.getCookie(url);
-            //Common.cookie = CookieStr;
-            Toast.makeText(activity,CookieStr,Toast.LENGTH_SHORT).show();
+            if (CookieStr != null) {
+                if(url.startsWith("https://pet-chain.baidu.com")) {
+                    SharedPreferences spf = getSharedPreferences("BaiduSPF", Context.MODE_PRIVATE);
+                    SharedPreferences.Editor editor = spf.edit();
+                    editor.putString("cookie", CookieStr);
+                    if(editor.commit()) {
+                        Toast.makeText(getApplicationContext(), "Cookieq存储完成", Toast.LENGTH_SHORT).show();
+                    }
+                }
+                Log.i(TAG+":url",url);
+                Log.i(TAG+":cookie", CookieStr);
+            }
             super.onPageFinished(view, url);
         }
+
+        /**
+         * 拦截 url 跳转,在里边添加点击链接跳转或者操作
+         */
+        public boolean shouldOverrideUrlLoading(WebView view, String url){
+            view.loadUrl(url);
+            return  true;
+        }
+        /**
+         * 加载错误的时候会回调，在其中可做错误处理，比如再请求加载一次，或者提示404的错误页面
+         */
+        public void onReceivedError(WebView view, int errorCode,String description, String failingUrl){
+            Log.e(TAG,"出错误了");
+        }
+        /**
+         * 当接收到https错误时，会回调此函数，在其中可以做错误处理
+         */
+        public void onReceivedSslError(WebView view, SslErrorHandler handler, SslError error){
+                Log.e(TAG,"出错误了");
+                super.onReceivedSslError(view,handler,error);
+        }
+        /**
+         * 在每一次请求资源时，都会通过这个函数来回调
+         */
+        public WebResourceResponse shouldInterceptRequest(WebView view,String url) {
+            return null;
+        }
+
+
     }
 
 }
--- app/src/main/java/ssw/com/myapplication/MainActivity.java
+++ app/src/main/java/ssw/com/myapplication/MainActivity.java
@@ -1,16 +1,214 @@
 package ssw.com.myapplication;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.text.method.ScrollingMovementMethod;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.StringWriter;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
+    private  Context mcontext;
+    TextView textView4;
+    EditText editText2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         TextView textView = findViewById(R.id.textView);
-        textView.setText(new HelloLibrary().sayHello());
+      //  textView.setText(new HelloLibrary().sayHello());
+        editText2 = findViewById(R.id.editText2);
+        textView4 = findViewById(R.id.textView4);
+        textView4.setMovementMethod(ScrollingMovementMethod.getInstance());
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,BaiduActivity.class);
+                startActivity(intent);
+            }
+        });
+        mcontext = this;
+        Button button1 = findViewById(R.id.button2);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                    new Thread(){
+                        public void run(){
 
+                            String url = editText2.getText().toString();
+
+                            try {
+                                sendPOSTRequest(getApplicationContext(),url,new HashMap<String, String>(),null);
+                            } catch (Exception e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }.start();
+            }
+        });
+        Button button3 = findViewById(R.id.button3);
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SharedPreferences spf = getSharedPreferences("BaiduSPF",0);
+                SharedPreferences.Editor editor = spf.edit();
+                editor.remove("cookie");
+                if(editor.commit()) {
+                    Toast.makeText(mcontext, "Cookieq清除完成", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+    /**
+     * 发送请求
+     * @param context
+     * @param baseUrl
+     * @param params
+     * @param handler
+     * @return
+     * @throws Exception
+     */
+    public  boolean sendPOSTRequest(Context context,String baseUrl, HashMap<String, String> params, Handler handler) throws Exception{
+
+       // mContext = context;
+
+        // 设置参数
+        byte[] entity = onParams(params);
+
+        // 设置请求链接
+        HttpURLConnection conn = onSetConn(mcontext, baseUrl, entity);
+
+        // 得到返回值
+        int responseCode = conn.getResponseCode();
+
+        if (HttpURLConnection.HTTP_OK == responseCode) {
+            String charset = getResponseCharset(conn.getContentType());
+            String result = getStreamAsString(conn.getInputStream(), charset);
+            textView4.setText(result);
+        } else {
+            textView4.setText("errorcode："+responseCode);
+        }
+
+        if(conn!=null){
+            conn.disconnect();
+        }
+        return false;
+    }
+  /**
+            * 参数拼接
+ * @return
+         * @throws UnsupportedEncodingException
+ */
+    private  byte[] onParams(HashMap<String, String> params) throws UnsupportedEncodingException {
+        if (params == null || params.isEmpty()){
+            params = new HashMap<>();
+        }
+        // 添加必须携带的参数信息
+      //  params.put("phone", getPhone());
+      //  params.put("password", getPassword());
+
+        //StringBuilder是用来组拼请求参数
+        StringBuilder sb = new StringBuilder();
+        if(params != null && params.size() != 0){
+            for (Map.Entry<String, String> entry : params.entrySet()) {
+                sb.append(entry.getKey()).append("=").append(URLEncoder.encode(entry.getValue(), "utf-8"));
+                sb.append("&");
+            }
+            sb.deleteCharAt(sb.length()-1);
+        }
+        return sb.toString().getBytes();
+    }
+    private  HttpURLConnection onSetConn(Context context, String baseUrl, byte[] entity) throws IOException {
+        URL url = new URL(baseUrl);
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setConnectTimeout(30000);
+        conn.setReadTimeout(5000);
+        // 设置以POST方式
+        conn.setRequestMethod("POST");
+        conn.setRequestProperty("Charset","utf-8");
+        //要向外输出数据，要设置这个
+        conn.setDoOutput(true);
+        conn.setDoInput(true);
+        conn.setUseCaches(false);
+        // 1、POST请求这个一定要设置
+        conn.setRequestProperty("Content-Type", "application/json");
+       // conn.setRequestProperty("Content-Length", "395931");
+        // 2、添加cookie信息
+        SharedPreferences spf = getSharedPreferences("BaiduSPF",Context.MODE_PRIVATE);
+
+        String temp_cookie = spf.getString("cookie","");
+        Log.e("test_cookie","temp_cookie is " + temp_cookie);
+        if (!TextUtils.isEmpty(temp_cookie)){
+            conn.setRequestProperty("Cookie",temp_cookie);
+        }
+        // 3、参数信息
+      //  OutputStream out = conn.getOutputStream();
+        //写入参数值
+      //  out.write(entity);
+        //刷新、关闭
+      //  out.flush();
+      //  out.close();
+        return conn;
+    }
+    private static String getStreamAsString(InputStream stream, String charset) throws IOException {
+        try {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(stream, charset));
+            StringWriter writer = new StringWriter();
+
+            char[] chars = new char[256];
+            int count = 0;
+            while ((count = reader.read(chars)) > 0) {
+                writer.write(chars, 0, count);
+            }
+
+            return writer.toString();
+        } finally {
+            if (stream != null) {
+                stream.close();
+            }
+        }
+    }
+    private  String getResponseCharset(String ctype) {
+        String charset = "UTF-8";
+        if (null != ctype && ctype != "") {
+            String[] params = ctype.split(";");
+            for (String param : params) {
+                param = param.trim();
+                if (param.startsWith("charset")) {
+                    String[] pair = param.split("=", 2);
+                    if (pair.length == 2) {
+                        if (null != pair[1] && "" != pair[1]) {
+                            charset = pair[1].trim();
+                        }
+                    }
+                    break;
+                }
+            }
+        }
+
+        return charset;
     }
 }
